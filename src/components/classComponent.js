7aeb3ee4d12d938bfa105af35e4851b62f948a48
Nathan Hamilton <nathan.a.hamilton@gmail.com>
2021-07-18 12:32:58 -0500

    build: comments example structure for class components

--- src/components/classComponent.js
+++ src/components/classComponent.js
@@ -1,6 +1,27 @@
 class LoanApproval extends React.Component {
+
+
     render() {
       return <h2></h2>;
     }
   }
   
+
+  // class Car extends React.Component {
+  //   render() {
+  //     return <h2>I am a Car!</h2>;
+  //   }
+  // }
+  
+  // class Garage extends React.Component {
+  //   render() {
+  //     return (
+  //       <div>
+  //       <h1>Who lives in my Garage?</h1>
+  //       <Car />
+  //       </div>
+  //     );
+  //   }
+  // }
+  
+  // ReactDOM.render(<Garage />, document.getElementById('root'));
